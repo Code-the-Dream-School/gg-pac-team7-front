a72af9ee24eec3719d37b270201898499c30f6e8
tobemari <domarkasmv@gmail.com>
2024-08-21 17:49:38 -0400

    added the sign up feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,16 @@
 import React, { useState, useEffect } from 'react';
 import { getAllData } from './util/index';
+import Register from './Register';
 
 const URL = 'http://localhost:8000/api/v1/';
 
 function App() {
-  
-  const [message, setMessage] = useState(''); 
-
-  useEffect(() => {
-
-    (async () => {
-      const myData = await getAllData(URL)
-      setMessage(myData.data);
-    })();
-      
-    return () => {
-      console.log('unmounting');
-    }
-
-  }, []);
 
   return (
     <>
-      <h1>{message}</h1>
+      <main className="App"> 
+        <Register /> 
+      </main>
     </>
   );
 
